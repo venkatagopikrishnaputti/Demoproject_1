91057295853ff8921598e1895508651c51b533d1
venkatagopikrishnaputti <venkatagopikrishnaputti@gmail.com>
2026-05-08 17:07:59 +0530

    java

---
+++ login.java
@@ -0,0 +1,7 @@
+// The file name must match the public class name (HelloWorld.java)
+public class HelloWorld {
+    // The main method is the entry point for every Java program
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Prints text to the screen
+    }
+}
